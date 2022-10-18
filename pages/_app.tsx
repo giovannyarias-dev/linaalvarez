87b562f29515470c5151d60a757664ef3464464d
Giovanny Arias <giovannyarias@Giovannys-MacBook-Pro.local>
2022-10-18 21:58:36 +0200

    Fix view height

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }, [])
+  
+
   return <Component {...pageProps} />
 }
 
